476e48d6fc8c2e850f9902edf6ef411c30c284ab
ysak-y <yoshiaki.0614@gmail.com>
2020-03-22 15:45:17 +0900

    Initial implementation for package's test

--- test/flutter_audio_capture_test.dart
+++ test/flutter_audio_capture_test.dart
@@ -1,23 +1,89 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:flutter_audio_capture/flutter_audio_capture.dart';
 
-void main() {
-  const MethodChannel channel = MethodChannel('flutter_audio_capture');
+class CallbackClass {
+  void onListen() {}
+  void onCancel() {}
+}
 
+class MockCallbackClass extends Mock implements CallbackClass {}
+
+void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  const AUDIO_CAPTURE_EVENT_CHANNEL_NAME =
+      "ymd.dev/audio_capture_event_channnel";
+  MethodChannel channel;
+  MockCallbackClass mock;
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    channel = MethodChannel(AUDIO_CAPTURE_EVENT_CHANNEL_NAME);
+    mock = MockCallbackClass();
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+    reset(mock);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterAudioCapture.platformVersion, '42');
+  void setupMockMethodCallHandlerWithMockFunctions(
+      Function mockOnListen, Function mockOnCancel) {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      switch (methodCall.method) {
+        case "listen":
+          {
+            mockOnListen();
+            break;
+          }
+        case "cancel":
+          {
+            mockOnCancel();
+            break;
+          }
+        default:
+          {
+            throw "Error occured in mock methodCallHandler";
+          }
+      }
+    });
+  }
+
+  test('listens stream of ymd.dev/audio_capture_event_channnel channel',
+      () async {
+    setupMockMethodCallHandlerWithMockFunctions(mock.onListen, mock.onCancel);
+
+    final client = FlutterAudioCapture();
+    await client.start((dynamic obj) => {}, (Object e) => {});
+    verify(mock.onListen()).called(1);
+    verifyNever(mock.onCancel());
+  });
+
+  test('cancels to listen stream ymd.dev/audio_capture_event_channnel channel',
+      () async {
+    setupMockMethodCallHandlerWithMockFunctions(mock.onListen, mock.onCancel);
+
+    final client = FlutterAudioCapture();
+    await client.start((dynamic obj) => {}, (Object e) => {});
+    await client.stop();
+    verify(mock.onCancel()).called(1);
+  });
+
+  test('nothing happens if stop() is called before start()', () async {
+    setupMockMethodCallHandlerWithMockFunctions(mock.onListen, mock.onCancel);
+
+    final client = FlutterAudioCapture();
+    await client.stop();
+    await client.start((dynamic obj) => {}, (Object e) => {});
+    verifyNever(mock.onCancel());
+  });
+
+  test('executes only once whether start() is called many times', () async {
+    setupMockMethodCallHandlerWithMockFunctions(mock.onListen, mock.onCancel);
+
+    final client = FlutterAudioCapture();
+    await client.stop();
+    await client.start((dynamic obj) => {}, (Object e) => {});
+    verifyNever(mock.onCancel());
   });
 }
