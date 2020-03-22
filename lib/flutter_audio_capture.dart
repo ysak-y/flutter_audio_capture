db0c0a285903a137b76e8bd31006fba5bbdf73ac
ysak-y <yoshiaki.0614@gmail.com>
2020-03-22 15:44:50 +0900

    Remove unnecessary properties from FlutterAudioCaptureClass

--- lib/flutter_audio_capture.dart
+++ lib/flutter_audio_capture.dart
@@ -2,19 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-const AUDIO_CAPTURE_METHOD_CHANNEL_NAME =
-    'ymd.dev/audio_capture_method_channel';
 const AUDIO_CAPTURE_EVENT_CHANNEL_NAME = "ymd.dev/audio_capture_event_channnel";
 
 class FlutterAudioCapture {
-  final MethodChannel _audioCaptureMethodChannel =
-      MethodChannel(AUDIO_CAPTURE_METHOD_CHANNEL_NAME);
   final EventChannel _audioCaptureEventChannel =
       EventChannel(AUDIO_CAPTURE_EVENT_CHANNEL_NAME);
   StreamSubscription _audioCaptureEventChannelSubscription;
 
   Future<void> start(Function listener, Function onError) async {
-    print(_audioCaptureEventChannelSubscription.toString());
     if (_audioCaptureEventChannelSubscription != null) return;
     _audioCaptureEventChannelSubscription = _audioCaptureEventChannel
         .receiveBroadcastStream()
